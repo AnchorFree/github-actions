2e824f78ff304311d360fed92af83a9e470617ae
Yaroslav Molochko <y.molochko@anchorfree.com>
2019-01-15 20:13:54 +0200

    Add vault-agent action

---
+++ vault-agent/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+export VAULT_AGENT_CONFIG_DIR=${VAULT_AGENT_CONFIG_DIR:-.}
+cd "${VAULT_AGENT_CONFIG_DIR}"
+
+cat << EOF > config.hcl
+pid_file = "${VAULT_AGENT_CONFIG_DIR}/vault-agent.pidfile"
+
+auto_auth {
+        method "approle" {
+                mount_path = "auth/approle"
+                config = {
+                        role_id_file_path = "${VAULT_AGENT_CONFIG_DIR}/role_id"
+                        secret_id_file_path = "${VAULT_AGENT_CONFIG_DIR}/secret_id"
+                        remove_secret_id_file_after_reading = "true"
+                }
+        }
+
+        sink "file" {
+                config = {
+                        path = "~/.vault-token"
+                }
+        }
+}
+EOF
+
+cat << EOF > ${VAULT_AGENT_CONFIG_DIR}/role_id
+${VAULT_ROLE_ID}
+EOF
+
+cat << EOF > ${VAULT_AGENT_CONFIG_DIR}/secret_id
+${VAULT_SECRET_ID}
+EOF
