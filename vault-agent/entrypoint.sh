43a81a4a99d42deb6bf988782e8de53d2b7f394c
Aleksey Zhukov <alex@izhukov.ru>
2020-06-11 01:53:58 +0300

    fix mode

--- vault-agent/entrypoint.sh
+++ vault-agent/entrypoint.sh
@@ -20,7 +20,7 @@ auto_auth {
         sink "file" {
                 config = {
                         path = "${VAULT_TOKEN_FILE}"
-			mode = "644"
+			mode = 0644
                 }
         }
 }
